1863a7d221a7b6486d04a5276aae7d8ddb24683d
mdashikalikhan <khan.ashik@gmail.com>
2025-05-17 15:22:47 +0600

    Star logic

--- src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
+++ src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import BookModel from "../../models/BookModel";
 import { SpinnerLoading } from "../utils/SpinnerLoading";
+import { StarsReview } from "../utils/StarsReview";
 
 export const BookCheckoutPage = () => {
 
@@ -86,6 +87,7 @@ export const BookCheckoutPage = () => {
                             <h2>{book?.title}</h2>
                             <h5 className="text-primary">{book?.author}</h5>
                             <p className="lead">{book?.description}</p>
+                            <StarsReview rating={.5} size={32}/>
                         </div>
                     </div>
                 </div>
